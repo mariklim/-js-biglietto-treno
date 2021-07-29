0330f503395e07a290889b04c38825d492caf846
mariklim <maria.klimenko889@gmail.com>
2021-07-29 15:45:28 +0200

    aggiunto le domande per chilometri de eta'

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// 1) chiedo all'utente il numero di chilometri che vuole percorrere
+// 2) chiedo all'utente l'età del passeggero
+// 3) se l'età comprede 18-64 anni allora il prezzo é pieno: 0.21 € x chilomitri richiesti
+// 4) se l'età è < 18 anni  é ridotto di 20% : ((0.21 € x chilomitri richiesti) / 100) x 80
+// 5) se l'età è > 65 anni  é ridotto di 40% : ((0.21 € x chilomitri richiesti) / 100) x 60
+// 6) stampo il prezzo finale con massimo due decimali, per indicare centesimi sul prezzo
+
+var chilometri = parseInt(prompt("Quanto è lungo il tuo viaggio? Scrivi il totale di chilomitri che vuoi percorrere"));
+
+console.log(chilometri);
+
+var eta = parseInt(prompt("Quanti anni hai?"));
+
+console.log(eta);
+
